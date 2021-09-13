fd2ba8196a2a303a8b5ab889ba2fe1e811313c15
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-09-13 10:17:00 +0000

    Prune

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 FC="/home/gmap/mrpm/marguina/install/gmkpack_support/wrapper/PGI217/mpif90  -mp -fPIC -Minfo -gopt -Mlarge_arrays -Mlist -traceback -Mnofma -Mbyteswapio -Mbackslash -O0 -g"
 CC="/home/gmap/mrpm/marguina/install/gmkpack_support/wrapper/PGI217/mpicc -g -O0"
 
-rm -f a.out
+rm -f a.out *.o *.mod
 
 $FC -c dist_grid_ctl_mod.F90
 $FC -c master.F90 edist_grid.F90
@@ -13,4 +13,4 @@ $FC *.o
 
 ulimit -s unlimited
 
-./a.out
+gdb -ex=run --args ./a.out
